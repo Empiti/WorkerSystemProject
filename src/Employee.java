a33f6758a3edcf08cf86fcdb3c6823c67faa59dc
Ksawery Kasprzycki <ksawery.kasprzyckix@gmail.com>
2023-05-16 12:33:34 +0200

    Dodałem funkcjonalność automatycznego dodawania id pracownikom

--- src/Employee.java
+++ src/Employee.java
@@ -1,2 +1,18 @@
 public class Employee {
+
+    private String firstName;
+    private String lastName;
+    private String position;
+    private int employeeID;
+    private int salary;
+    private static int nextEmployeeID;
+
+    public Employee(String firstName, String lastName, String position, int employeeID, int salary) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.position = position;
+        this.employeeID = nextEmployeeID;
+        this.salary = salary;
+        nextEmployeeID++;
+    }
 }
